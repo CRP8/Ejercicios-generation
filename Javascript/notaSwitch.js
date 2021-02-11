1f7c55cc75ef05f3fe577ed9ba5cfb27cd4f9fb8
CRP8 <CRP8@users.noreply.github.com>
2021-02-11 20:02:04 +0100

    Create notaSwitch.js

---
+++ Javascript/notaSwitch.js
@@ -0,0 +1,37 @@
+
+  var nota = parseFloat(prompt("Introduce la nota: "))
+
+  switch (true) {
+
+  case (nota < 0):
+    console.log("introduce un numero mayor que 0");
+    break;
+  case (nota < 3):
+    console.log("Muy deficiente");
+    break;
+  case (nota < 5):
+    console.log("Insuficiente");
+    break;
+  case (nota < 6):
+    console.log("Suficiente");
+    break;
+  case (nota < 6):
+    console.log("Suficiente");
+    break;
+  case (nota < 7):
+    console.log("Bien");
+    break;
+  case (nota < 9):
+    console.log("Notable");
+    break;
+  case (nota <= 10):
+    console.log("Sobresaliente");
+    break;
+    
+    
+
+  default:
+    console.log('introduce un numero menor que 10');
+}
+
+
